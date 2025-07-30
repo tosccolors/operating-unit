6c2ca12b2ad88747bd09e0747f09e410871a70ed
Deepa Venkatesh <deepa4lok@users.noreply.github.com>
2025-07-30 11:18:18 +0530

    Revert "[ADD][983] account_invoice_merge_operating_unit"

--- mail_template_operating_unit_default/models/__init__.py
+++ mail_template_operating_unit_default/models/__init__.py
@@ -3,3 +3,4 @@
 
 from . import sale
 from . import invoice
+
--- mail_template_operating_unit_default/wizard/invoice_send.py
+++ mail_template_operating_unit_default/wizard/invoice_send.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2024 Willem Hulsof
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+import base64
 
 
 class AccountInvoiceSend(models.TransientModel):
-    _inherit = "account.invoice.send"
-    _description = "Account Invoice Send"
+    _inherit = 'account.invoice.send'
+    _description = 'Account Invoice Send'
 
     # Overridden:
     template_id = fields.Many2one(
-        "mail.template",
-        "Use template",
-        index=True,
-        domain="[('model', '=', 'account.move'), ('operating_unit_id','=',operating_unit_id)]",
+        'mail.template', 'Use template', index=True,
+        domain="[('model', '=', 'account.move'), ('operating_unit_id','=',operating_unit_id)]"
     )
 
     operating_unit_id = fields.Many2one(
