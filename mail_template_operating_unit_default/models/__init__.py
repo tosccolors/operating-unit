f342c0d0174f4fc8755072deb2bc1c9e45b0f26d
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-07-30 09:37:32 +0530

    [ADD][983] account_invoice_merge_operating_unit

--- mail_template_operating_unit_default/models/__init__.py
+++ mail_template_operating_unit_default/models/__init__.py
@@ -3,4 +3,3 @@
 
 from . import sale
 from . import invoice
-
--- mail_template_operating_unit_default/wizard/mail_compose.py
+++ mail_template_operating_unit_default/wizard/mail_compose.py
@@ -1,15 +1,19 @@
 # Copyright (C) 2024 Willem Hulsof
 # -*- coding: utf-8 -*-
 
-from odoo import _, api, fields, models, tools
+from odoo import api, fields, models
+
 
 class MailComposer(models.TransientModel):
-    _inherit = 'mail.compose.message'
+    _inherit = "mail.compose.message"
 
-    #Overridden:
+    # Overridden:
     template_id = fields.Many2one(
-        'mail.template', 'Use template', index=True,
-        domain="[('model', '=', model), ('operating_unit_id', '=', operating_unit_id)]")
+        "mail.template",
+        "Use template",
+        index=True,
+        domain="[('model', '=', model), ('operating_unit_id', '=', operating_unit_id)]",
+    )
 
     operating_unit_id = fields.Many2one(
         comodel_name="operating.unit",
@@ -19,19 +23,17 @@ class MailComposer(models.TransientModel):
         ),
     )
 
-
     @api.model
     def default_get(self, fields):
         result = super(MailComposer, self).default_get(fields)
 
-        model = result.get('model', False)
-        res_id = result.get('res_id', False)
+        model = result.get("model", False)
+        res_id = result.get("res_id", False)
         if model and res_id:
             Obj = self.env[model].browse(res_id)
             try:
-                result['operating_unit_id'] = Obj.operating_unit_id.id
+                result["operating_unit_id"] = Obj.operating_unit_id.id
             except:
                 pass
 
         return result
-
